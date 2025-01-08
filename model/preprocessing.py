c436a9a63d472cb346ef9cc980076a5fd3544b6d
Heidy Hernandez <hheidy463@gmail.com>
2025-01-08 13:42:34 -0800

    Updated tokenizer and dataset code

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -45,7 +45,7 @@ class TextProcessor(BaseEstimator, TransformerMixin):
     def __init__(self, preprocess_func=None):
         self.preprocess_func = preprocess_func if preprocess_func else preprocess_text
 
-    def fit (self, X, y=None):
+    def fit(self, X, y=None):
         """
         Update if needed
         """
--- model/train_model.py
+++ model/train_model.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import torch
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from preprocessing import TextProcessor, preprocess_text
+from transformers import DistilBertTokenizerFast
+
 """
 1. Load Data
 
@@ -21,7 +24,7 @@ y = df["label"].tolist() # List of binary labels
 
 Split data into training and testing sets (80/20)
 """
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train_raw, X_test_raw, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
 """
@@ -30,3 +33,35 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 Use TextProcessor class
 """
 text_processor = TextProcessor(preprocess_func=preprocess_text)
+X_train_clean = text_processor.transform(pd.Series(X_train_raw)).tolist()
+X_test_clean = text_processor.transform(pd.Series(X_test_raw)).tolist()
+
+"""
+4. Tokenize with DistilBERT
+"""
+tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+
+train_encodings = tokenizer(
+    X_train_clean, truncation=True, padding=True, max_length=128
+)
+
+test_encodings = tokenizer(
+    X_test_clean, truncation=True, padding=True, max_length=128
+)
+
+"""
+5. Create PyTorch Datasets
+"""
+class AvottractionDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __get_items__(self, idx):
+        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        # Binary Classification - Change as needed
+        item['labels'] = torch.tensor(self.labels[idx])
+        return item
+
+    def __len__(self):
+        return len(self.labels)
